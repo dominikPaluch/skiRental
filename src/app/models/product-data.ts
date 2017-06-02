b9f55a5d20ea7f54bd5a0313e78ac216e64879c4
Ada-HP\Ada <ada.frykowska@gmail.com>
2017-06-02 14:22:28 +0200

    Ustawienie wartości minimalnej na 0 dla ceny i liczby rezerwowanego sprzętu. Modyfikacja opisów sprzętu i komunikatu powitalnego na górnym pasku.

--- src/app/models/product-data.ts
+++ src/app/models/product-data.ts
@@ -2,15 +2,19 @@ import { Product } from './Product';
 
   export const PRODUCTS: Product[] = [
       { id: 1, name: 'Narty', src: '/assets/img/skis/big_a48c82e99ec94d4d967ff56b84e2bb50.jpg', price: 99.99, size: 170,
-      info: 'The Nexus S is a smartphone co-developed by Google and Samsung and manufactured by Samsung Electronics for release in 2010. It was the first smartphone to use the Android 2.3 Gingerbread operating system, and the first Android device to support Near Field Communication (NFC).' },
+        info: 'Narty idealne do ...'},
       { id: 2, name: 'Narty biegowe', src: '/assets/img/skis/meskie-narty-biegowe.jpg', price: 109.99, size: 180,
-      info: 'The Dell Venue is a line of Android smartphones and tablets manufactured by Dell. The first Dell Venue was released for both T-Mobile and AT&T in the United States, and for KT in South Korea. It was the second Dell smartphone to be released in the US and features the Dell Stage UI also found on the Dell Streak line of tablets.' },
+        info: 'Narty idealne do ...'},
       { id: 3, name: 'Narty atomic redster', src: '/assets/img/skis/narty-atomic-redster-doubledack-xt.jpg', price: 89.99, size: 180,
-      info: 'The Motorola Droid 2 is the fifth phone in Verizon Droid line. It runs the Android operating system by Google, and can run Flash Player 10.1. It comes with 8 GB of internal memory and is shipped with an additional 8 GB SDHC card, upgradable to 32 GB. It has a 3.7 in display and a 5-megapixel camera.' },
+        info: 'Narty idealne do ...'},
       { id: 4, name: 'Narty biegowe fischer', src: '/assets/img/skis/narty-biegowki-klasyk-fischer_n43616_fibre_crown.jpg', price: 129.99, size: 190,
-      info: 'The Motorola Droid Pro is an Android-based smartphone manufactured by Motorola and released on November 18, 2010, for Verizon Wireless and June 5, 2011, for Sprint. These devices are available for Verizon, Sprint and Boost Mobile in the United States, and are designed for business users.' },
+        info: 'Narty idealne do ...'},
       { id: 5, name: 'Narty local hero', src: '/assets/img/skis/narty-local-hero-jr.jpg', price: 119.99, size: 170,
-      info: 'The Motorola Bravo has a nice 3.7 inch WVGA display, a slim build, and a multimedia-friendly feature set that includes DLNA support. It has good call quality as well. The Motorola Bravo is a midrange Android smartphone with solid multimedia features.'},
+        info: 'Narty idealne do ...'},
       { id: 6, name: 'Narty salomon x-race', src: '/assets/img/skis/narty-salomon-x-race-sw.jpg', price: 139.99, size: 160,
-      info: 'The Motorola Defy, also known as Motorola Defy A8210 & MB525, is an Android-based smartphone from Motorola. It fills a unique market segment, by being one of the few small, IP67 rated smartphones available. It is water-resistant, dust-resistant, and has an impact-resistant screen.' },
+        info: 'Narty idealne do ...'},
+      { id: 7, name: 'Narty', src: '/assets/img/skis/narty-volkl-nanuq.jpg', price: 109.99, size: 165,
+        info: 'Narty idealne do ...'},
+      { id: 8, name: 'Narty biegowe rekreacyjne', src: '/assets/img/skis/nartybiegowe_rekreacyjne.jpg', price: 119.99, size: 175,
+        info: 'Narty idealne do ...'}
   ]
