37389bd8045d4a746009f5ba4a44a4f2820d5541
Ada-HP\Ada <ada.frykowska@gmail.com>
2017-06-03 12:05:40 +0200

    Ukrycie przycisku "Moje rezerwacje" dla niezalogowanych użytkowników.

--- src/app/components/left-menu/left-menu.component.ts
+++ src/app/components/left-menu/left-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../registration/_models/index';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-left-menu',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftMenuComponent implements OnInit {
 
-  constructor() { }
+    currentUser: User;
+
+    constructor(private router: Router) {
+
+
+      router.events.subscribe(() => {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+      });
+    }
 
   ngOnInit() {
   }
