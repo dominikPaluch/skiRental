76caf084eec0e5fc49d485f75d6afa45c5eb729d
Ada-HP\Ada <ada.frykowska@gmail.com>
2017-06-10 17:24:55 +0200

    Pogrubienie ceny w widoku rezerwacji.

--- src/app/models/reservation-removed-data.ts
+++ src/app/models/reservation-removed-data.ts
@@ -1,7 +1,7 @@
 import { Reservation } from './Reservation';
 
   export const ReservationsRemoved: Reservation[] = [
-        { id: 2, price: 689.94, startDate: '19-12-2017', endDate: '22-12-2017', daysReserved: 3, daysLeft: 8, products: [
+        { id: 2, price: 689.94, startDate: '19-12-2017', endDate: '22-12-2017', daysReserved: 4, daysLeft: 8, products: [
           { id: 5, name: 'Narty local hero', src: '/assets/img/skis/narty-local-hero-jr.jpg', price: 109.99, size: 170,
             info: 'Narty idealne do ...'},
           { id: 8, name: 'Narty biegowe rekreacyjne', src: '/assets/img/skis/nartybiegowe_rekreacyjne.jpg', price: 119.99, size: 175,
